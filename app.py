fce2004a3cd8212999966e3e9bc9a23f5ef6be49
sarathoff <sarathramesh.official@gmail.com>
2024-08-08 14:15:43 +0530

    setup process

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
 
-def summarize_audio(audio_file_path):
+def analyse_audio(audio_file_path):
     """analyse the speech audio using Google's Generative API."""
     model = genai.GenerativeModel("models/gemini-1.5-pro-latest")
     audio_file = genai.upload_file(path=audio_file_path)
@@ -24,21 +24,43 @@ def summarize_audio(audio_file_path):
     )
     return response.text
 
+def summarize_audio(audio_file_path):
+    """summarise the audio using Google's Generative API."""
+    model = genai.GenerativeModel("models/gemini-1.5-pro-latest")
+    audio_file = genai.upload_file(path=audio_file_path)
+    response = model.generate_content(
+        [
+            "Please summarise the speech and give small notes to speak again for JAM Speech practice, also suggest some vocabulary to add in another speech",
+            audio_file
+        ]
+    )
+    return response.text
+
 def save_audio_file(audio_bytes):
     """Save recorded audio bytes to a temporary file and return the path."""
     try:
         with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as tmp_file:
             tmp_file.write(audio_bytes)
-            return tmp_file.name
+            tmp_file_path = tmp_file.name
+            return tmp_file_path
     except Exception as e:
         st.error(f"Error handling recorded audio: {e}")
         return None
+
+def get_today_topic():
+    """Generate a topic for Just a Minute practice using Gemini API."""
+    model = genai.GenerativeModel("models/gemini-1.5-pro-latest")
+    response = model.generate_content(
+        ["Please provide a topic for a Just a Minute practice session make it easier"]
+    )
+    return response.text
 
 def start_chat_session():
     """Initialize chat session."""
     return genai.GenerativeModel('gemini-1.5-pro').start_chat(history=[])
 
 # Configure Streamlit page settings
+
 st.set_page_config(
     page_title="Captain Jam - Communication Coach",
     page_icon=":brain:",  # Favicon emoji
@@ -50,10 +72,16 @@ if "chat_session" not in st.session_state:
     st.session_state.chat_session = start_chat_session()
 
 # Display the app title and description
-st.title("Captain Jam")
+st.title("Captain Jam -English Communication Coach")
 st.write("""
-    Record or upload an audio file to get a concise summary, then chat with Gemini-Pro about the summary.
+   Your Co-captain to Supercharge Your English and Communication - Speak. Learn. Grow.
 """)
+
+# Display the topic for Just a Minute practice
+
+with st.spinner('Generating topic...'):
+    today_topic = get_today_topic()
+    st.write(today_topic)
 
 # Record audio
 audio_bytes = audio_recorder(
@@ -64,14 +92,17 @@ audio_bytes = audio_recorder(
     energy_threshold=(-1.0, 1.0),
     pause_threshold=60.0,
 )
-
 if audio_bytes:
     audio_path = save_audio_file(audio_bytes)
-    st.audio(audio_path, format="audio/wav")
+    # Check if the file exists and is accessible
+    if audio_path and os.path.exists(audio_path):
+        st.audio(audio_path, format="audio/wav")
+    else:
+        st.error("Audio file could not be saved or accessed.")
 
     if st.button('Analyse Recorded Audio'):
-        with st.spinner('Analysizing...'):
-            summary_text = summarize_audio(audio_path)
+        with st.spinner('Analyzing...'), st.spinner('Vocabulary checking...'):
+            summary_text = analyse_audio(audio_path)
             st.info(summary_text)
 
             # Start chat with the summary
@@ -79,14 +110,15 @@ if audio_bytes:
                 st.session_state.chat_session.send_message(f"The summary report of speech analysis is: {summary_text}")
 
 # Upload audio
-uploaded_file = st.file_uploader("Upload Audio File", type=['wav', 'mp3'])
+uploaded_file = st.file_uploader("Upload Audio File", type=['wav', 'mp3', 'mpeg'])
 if uploaded_file is not None:
     audio_path = save_audio_file(uploaded_file.getvalue())  # Save the uploaded file and get the path
     st.audio(audio_path)
 
+
     if st.button('Analyse Uploaded Audio'):
         with st.spinner('Analyzing...'):
-            summary_text = summarize_audio(audio_path)
+            summary_text = analyse_audio(audio_path)
             st.info(summary_text)
 
             # Start chat with the summary
